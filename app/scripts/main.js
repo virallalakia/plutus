c68ae68ff91d354ec61b980495b41c3fa655c89e
Dhwaneet Bhatt <dhwaneetbhatt@gmail.com>
2013-09-07 23:18:56 +0530

    Modified the dependancy versions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,11 @@ require.config({
    paths: {
        "jquery": "lib/jquery/jquery",
        "ember": "lib/ember/ember",
+       "ember-data": "lib/ember-data-shim/ember-data",
        "handlebars": "lib/handlebars/handlebars"
    }
 });
 
-require([], function() {
+require([], function () {
     console.log('Hello World');
 });
